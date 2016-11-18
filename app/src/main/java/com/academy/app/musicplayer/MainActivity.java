c86c621c9a84ff290c2b2b8f668cf601a783a25f
NaThAn H <stuff@whatever.com>
2016-11-18 14:01:06 -0800

    addedd buttons

--- app/src/main/java/com/academy/app/musicplayer/MainActivity.java
+++ app/src/main/java/com/academy/app/musicplayer/MainActivity.java
@@ -1,13 +1,42 @@
 package com.academy.app.musicplayer;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer s1;
+    Button playButtonVar;
+    Button pauseButtonVar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        s1 = MediaPlayer.create(this,R.raw.song1);
+        playButtonVar = (Button) findViewById(R.id.button2);
+        pauseButtonVar = (Button) findViewById(R.id.button1);
+    }
+
+    public void playButton(View view){
+        pauseButtonVar.setEnabled(true);
+        playButtonVar.setEnabled(false);
+        s1.start();
+    }
+    public void pauseButton(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "Paused";
+        int duration = Toast.LENGTH_SHORT;
+        Toast myMessage= Toast.makeText(context, text, duration);
+        myMessage.show();
+        s1.pause();
+        pauseButtonVar.setEnabled(false);
+        playButtonVar.setEnabled(true);
     }
 }
