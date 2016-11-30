8938debfead9e02649da65b8d416bd2a423331b1
NaThAn H <stuff@whatever.com>
2016-11-30 14:03:03 -0800

    m2

--- app/src/main/java/com/academy/app/musicplayer/MainActivity.java
+++ app/src/main/java/com/academy/app/musicplayer/MainActivity.java
@@ -2,6 +2,7 @@ package com.academy.app.musicplayer;
 
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +10,13 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Handler myHandler = new Handler();
 
     private MediaPlayer s1;
     Button playButtonVar;
     Button pauseButtonVar;
+    int finalTimeMS;
+    int currentTimeMS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,24 @@ public class MainActivity extends AppCompatActivity {
         s1 = MediaPlayer.create(this,R.raw.song1);
         playButtonVar = (Button) findViewById(R.id.button2);
         pauseButtonVar = (Button) findViewById(R.id.button1);
+
+        endMinutes = (int) (finalTimeMS / 1000 / 60);
+        endSeconds = ((int) (finalTimeMS / 1000)) %60;
+        currentMinutes =(int) (currentTimeMS/1000/60);
+        currentSeconds = ((int)(currentTimeMS/1000)) %60;
+
+        finalTimeMS = s1.getDuration();
+        currentTimeMS = s1.getCurrentPosition();
+
+        endTimeViewVar.setText("Text");
+        myHandler.postDelayed(UpdateSongTime, 100);
     }
+
+    private Runnable UpdateSongTime = new Runnable() {
+        public void run() {
+            myHandler.postDelayed(this, 100);
+        }
+    };
 
     public void playButton(View view){
         pauseButtonVar.setEnabled(true);
