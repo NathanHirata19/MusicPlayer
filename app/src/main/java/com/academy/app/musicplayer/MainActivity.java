121471aafe6c115db6ef345f857282a16896d25e
NaThAn H <stuff@whatever.com>
2016-12-02 14:02:50 -0800

    error

--- app/src/main/java/com/academy/app/musicplayer/MainActivity.java
+++ app/src/main/java/com/academy/app/musicplayer/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,10 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer s1;
 
-    private TextView endTimeViewVar;
+    private SeekBar mySongBarVar;
     Button playButtonVar;
     Button pauseButtonVar;
-    Button stopButton;
     Button rewind;
     Button forward;
     double finalTimeMS;
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     int currentSeconds;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,29 +43,48 @@ public class MainActivity extends AppCompatActivity {
         s1 = MediaPlayer.create(this,R.raw.song1);
         playButtonVar = (Button) findViewById(R.id.button2);
         pauseButtonVar = (Button) findViewById(R.id.button1);
-        stopButton = (Button) findViewById(R.id.stopButton);
         forward = (Button) findViewById(R.id.forward);
         rewind = (Button) findViewById(R.id.rewind);
         endMinutesView = (TextView) findViewById(R.id.fm);
         endSecondsView = (TextView) findViewById(R.id.fs);
         currentMinutesView = (TextView) findViewById(R.id.cm);
         currentSecondsView = (TextView) findViewById(R.id.cs);
+        mySongBarVar= (SeekBar) findViewById(R.id.mySongBar);
+
+        mySongBarVar.setMax((int) finalTimeMS);
+        mySongBarVar.setProgress((int) currentTimeMS);
 
         endMinutes = (int) (finalTimeMS / 1000 / 60);
         endSeconds = ((int) (finalTimeMS / 1000)) %60;
         currentMinutes =(int) (currentTimeMS/1000/60);
         currentSeconds = ((int)(currentTimeMS/1000)) %60;
 
-        endMinutesView.setText("1");
-        endSecondsView.setText("2");
-        currentMinutesView.setText("3");
-        currentSecondsView.setText("4");
+        endMinutesView.setText("" + endMinutes);
+        endSecondsView.setText("" + endSeconds);
+        currentMinutesView.setText("" + currentMinutes);
+        currentSecondsView.setText("" + currentSeconds);
 
 
         finalTimeMS = s1.getDuration();
         currentTimeMS = s1.getCurrentPosition();
 
         myHandler.postDelayed(UpdateSongTime, 100);
+
+
+        mySongBarVar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
 
     private Runnable UpdateSongTime = new Runnable() {
@@ -88,15 +108,16 @@ public class MainActivity extends AppCompatActivity {
         pauseButtonVar.setEnabled(false);
         playButtonVar.setEnabled(true);
     }
-
     public void rewind(View view){
         s1.seekTo( (int) (currentTimeMS - 5000) );
     }
 
     public void forward(View view){
-        s1.seekTo( (int) (currentTimeMS + 5000) );
-    }
-    public void stopButton(View view){
         s1.seekTo( (int) (currentTimeMS - 5000) );
     }
+    public void stopButton(View view){
+            s1.seekTo((int) (currentTimeMS - (currentMinutes + currentSeconds)));
+        s1.pause();
+        }
+
 }
