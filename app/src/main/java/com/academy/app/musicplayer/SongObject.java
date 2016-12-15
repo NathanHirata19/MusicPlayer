552a628d7a22b3299444cbeecce393b3097bfd6f
NaThAn H <stuff@whatever.com>
2016-12-14 23:14:48 -0800

    all milestones errors everywhere doe

--- app/src/main/java/com/academy/app/musicplayer/SongObject.java
+++ app/src/main/java/com/academy/app/musicplayer/SongObject.java
@@ -1,5 +1,8 @@
 package com.academy.app.musicplayer;
 
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+
 public class SongObject {
     public int songID;
     public String title;
